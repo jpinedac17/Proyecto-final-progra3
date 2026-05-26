e5a1aaf80bef11e30153834c38c96d671d6e52bd
Deivid Alberto Guerra Carpio <dguerrac7@miumg.edu.gt>
2026-05-26 03:09:04 -0600

    Corrige configuración de perfiles y datos de prueba

--- backend/src/main/resources/data-postgres.sql
+++ backend/src/main/resources/data-postgres.sql
@@ -1,13 +1,1 @@
-INSERT INTO nodes (id, value, parent_id) VALUES
-(1, 'Guatemala', NULL),
-
-(2, 'Petén', 1),
-(3, 'Sacatepéquez', 1),
-
-(4, 'Flores', 2),
-(5, 'San Benito', 2),
-
-(6, 'Mixco', 3),
-
-(7, 'Aldea El Remate', 4),
-(8, 'Aldea Lo de Bran', 6);
+SELECT 1;
